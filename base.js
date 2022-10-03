053b987e887157cd8c7de9065a5c059781118eaf
Micaela Zerpa <micazerpa14@gmail.com>
2022-10-03 15:40:38 -0300

    herencia de header y footer

---
+++ base.js
@@ -0,0 +1,20 @@
+document.getElementById("idheader").innerHTML = `
+<div class="header-logo">
+            <img src="img/logo.png" alt="logo"></div> 
+        <nav class="header-menubar">
+                
+                    <div><a href="index.html">Inicio</a></div>
+                    <div><a href="recetas.html">Recetas</a> </div>
+                    <div><a href="http://somos.html">Quienes somos</a></div>
+                    <div><a href="contacto.html">Contacto</a></div>
+                    <div> <input type="text" placeholder="Search.."></div>
+            
+          
+        </nav>
+
+`
+
+document.getElementById("idfooter").innerHTML = ` 
+<h2>elemtos footer</h2>
+
+`
