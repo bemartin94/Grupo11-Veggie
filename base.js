5cb4d0a7af1917b7b22fc6c8ee95e5c16685a1ee
Belen Martin <bemartin@hotmail.com.ar>
2022-10-03 18:52:50 -0300

    barra de navegacion fija

--- base.js
+++ base.js
@@ -4,7 +4,12 @@ document.getElementById("idheader").innerHTML = `
         <div class="header-logo">
         <img src="img/logo.png" alt="logo"></div> 
                     <div><a href="index.html">Inicio</a></div>
-                    <div><a href="recetas.html">Recetas</a> </div>
+                    <div class="dropdown">      <button class="dropbtn">Recetas    <i class="fa fa-caret-down"></i>       </button>
+                    <div class="dropdown-content">
+                      <a href="#">Link 1</a><br>
+                      <a href="#">Link 2</a><br>
+                      <a href="#">Link 3</a><br>
+                    </div></div>
                     <div><a href="http://somos.html">Quienes somos</a></div>
                     <div><a href="contacto.html">Contacto</a></div>
                     <div> <input type="text" placeholder="Search.."></div>
@@ -13,6 +18,7 @@ document.getElementById("idheader").innerHTML = `
         </nav>
 
 `
+
 
 document.getElementById("idfooter").innerHTML = ` 
 <h2>elemtos footer</h2>
