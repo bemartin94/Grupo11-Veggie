1a00a8d3bd8312d0254c05eb25354ee9908c5753
Belen Martin <bemartin@hotmail.com.ar>
2022-10-13 13:07:32 -0300

    cambios en index

--- base.js
+++ base.js
@@ -11,9 +11,9 @@ document.getElementById("idheader").innerHTML = `
                     <div><a href="index.html">Inicio</a></div>
                     <div class="dropdown">      <button class="dropbtn">Recetas    <i class="fa fa-caret-down"></i>       </button>
                     <div class="dropdown-content">
-                      <a href="#">Link 1</a><br>
-                      <a href="#">Link 2</a><br>
-                      <a href="#">Link 3</a><br>
+                      <a href="#">Guisos</a><br>
+                      <a href="#">Panes</a><br>
+                      <a href="#">Postres</a><br>
                     </div></div>
                     <div><a href="http://somos.html">Quienes somos</a></div>
                     <div><a href="contacto.html">Contacto</a></div></div>
