a6c5a4aa7dd57e6283c3ba057d3298e5b2d6dde2
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-10-15 16:37:37 -0300

    feat:envio de mail

--- contacto.js
+++ contacto.js
@@ -5,11 +5,13 @@
     const lastName = document.getElementById('lastname');
     const email = document.getElementById('email');
     const comment = document.getElementById('comment');
+    let error = false;
 
     let validateName = (e) => {
         if(firstName.value === ""){
             alert('Completa el nombre');
             e.preventDefault();
+            error = true;
         }
     }
 
@@ -17,6 +19,7 @@
         if(lastName.value === ""){
             alert('Completa el apellido');
             e.preventDefault();
+            error = true;
         }
     }
 
@@ -24,6 +27,7 @@
         if(email.validity.typeMismatch){
             alert('Email incorrecto');
             e.preventDefault();
+            error = true;
         }
     }
 
@@ -31,14 +35,30 @@
         if(comment.value === ""){
             alert('Completa el comentario');
             e.preventDefault();
+            error = true;
         }
     }
 
-    let validate = (e) => {
+    let validate = async (e) => {
         validateName(e);
         validateLastName(e);
         validateEmail(e);
         validateComment(e);
+        if(!error){
+            e.preventDefault();
+            const veggieForm = new FormData(this);
+            const response = await fetch(this.action, {
+                method: this.method,
+                body: veggieForm,
+                headers:{
+                    'Accept': 'application/json'
+                }
+            });
+            if(respones.ok){
+                this.reset();
+                alert('Gracias por su comentario');
+            }
+        }
     }
 
     form.addEventListener('submit',validate);
