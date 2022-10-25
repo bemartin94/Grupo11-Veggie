1225015ff5b4b317a32f6a24f07dca5f728c10ee
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-10-24 23:44:18 -0300

    quienes somos

--- base.js
+++ base.js
@@ -16,7 +16,7 @@ document.getElementById("idheader").innerHTML = /*html */`
                       <a href="#">Panes</a><br>
                       <a href="#">Postres</a><br>
                     </div></div>
-                    <div><a href="http://somos.html">Quienes somos</a></div>
+                    <div><a href="somos.html">Quienes somos</a></div>
                     <div><a href="contacto.html">Contacto</a></div></div>
                     <div class="buscador"> <input type="text" placeholder="Buscar..." class="buscador" id="buscador">     <button type="enviar"><i class="fa fa-search"></i></button></div> 
                     <div class="buscador-mobile" id="buscador-mobile"><button type="submit" id="buscar"><i class="fa fa-search"></i></button></div> 
